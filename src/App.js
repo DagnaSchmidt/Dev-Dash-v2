2a4f72b0ac545e05a7e2142d3b88ff60340ed828
Dagna Schmidt <dagdagi889@gmail.com>
2023-03-27 11:57:42 +0200

    router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes,  } from "react-router-dom";
 import { configureStore } from "redux";
 import reducer from "./reducer";
 import { Provider } from "react-redux";
+import Info from "./Components/Info";
+import BottomBar from "./Components/BottomBar";
+import Weather from './Components/Weather/Weather';
 
 const store = configureStore(
   reducer,
@@ -14,7 +17,20 @@ const store = configureStore(
 function App() {
   return (
     <Provider store={store}>
+      <div className='app'>
+        <Info />
+        <main>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/weather' element={<Weather />} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+        <div className='icons'>
 
+        </div>
+        <BottomBar />
+      </div>
     </Provider>
   );
 }
---
+++ src/Components/Info.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Info = () => {
+  return (
+    <header>Info</header>
+  )
+}
+
+export default Info;
