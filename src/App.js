dbe62fc694faa1366c90945abae2a4a9cde79b72
Dagna Schmidt <dagdagi889@gmail.com>
2023-03-28 09:22:08 +0200

    reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes,  } from "react-router-dom";
-import { configureStore } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducer";
 import { Provider } from "react-redux";
 import Info from "./Components/Info";
 import BottomBar from "./Components/BottomBar";
 import Weather from './Components/Weather/Weather';
 
-const store = configureStore(
+const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
