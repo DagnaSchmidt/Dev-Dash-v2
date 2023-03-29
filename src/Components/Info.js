99e49c0daf15cb2dd9153f5c9dd072f43b329206
Dagna Schmidt <dagdagi889@gmail.com>
2023-03-29 16:27:09 +0200

    josefin sans font added

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -4,7 +4,17 @@ import '../Styles/Components_Styles/Info.css';
 const Info = () => {
   return (
     <header className='info'>
-        Info
+        <div className='info__top'>
+          <p className='info__top__greeting'>
+            Greeting hardcoded
+          </p>
+          <p className='info__top__username display-small'>
+            Username hardcoded
+          </p>
+        </div>
+        <div className='info__bottom'>
+
+        </div>
     </header>
   )
 }
