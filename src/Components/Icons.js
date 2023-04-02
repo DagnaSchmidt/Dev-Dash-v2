5936a2bc7d0d6eca1ea09713bcbfd23d449f0162
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-02 18:17:35 +0200

    widget list

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { IoCalculator, IoCopy, IoCalendar, IoBarbell, IoCaretForwardCircle, IoCellular, IoFastFood, IoLanguage, IoFilm, IoLibrary, IoDice, IoGameController, IoHeadset } from "react-icons/io5";
+import { IoMdPartlySunny } from "react-icons/io";
 import '../Styles/Components_Styles/Icons.css';
 
 const Icons = ( {activeWidget} ) => {
+
+  const allWidgets = [
+    ['calculator', 'IoCalculator'],
+    ['notes', 'IoCopy'],
+    ['calendar', 'IoCalendar'],
+    ['weather', 'IoMdPartlySunny'],
+    ['time tracker', 'IoCaretForwardCircle'],
+    ['expenses', 'IoCellular'],
+    ['calorie counter', 'IoFastFood'],
+    ['translator', 'IoLanguage'],
+    ['movies', 'IoFilm'],
+    ['books', 'IoLibrary'],
+    ['board games', 'IoDice'],
+    ['music', 'IoHeadset'],
+    ['video games', 'IoGameController'],
+    ['exercises', 'IoBarbell'],
+    ['drink recipes', 'IoBeer']
+  ]
+
   return (
     <section className={`icons ${activeWidget === 'none' && 'icons-closed'}`}>
         Icons
