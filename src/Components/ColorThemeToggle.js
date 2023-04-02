c4ecb93f1335a2306659c4e9967e3e9d699c42fe
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-02 11:59:40 +0200

    bottom-bar widget content

---
+++ src/Components/ColorThemeToggle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ColorThemeToggle = () => {
+  return (
+    <div className='color-theme-toggle'>
+        <div className='color-theme-toggle__btns'>
+            <div className='bg'>
+
+            </div>
+            <div className='black'>
+            <div className='dot'></div>
+            </div>
+            <div className='color'>
+            <div className='dot'></div>
+            </div>
+        </div>
+        <p className='color-theme-toggle__title'>
+            color theme
+        </p>
+    </div>
+  )
+}
+
+export default ColorThemeToggle
