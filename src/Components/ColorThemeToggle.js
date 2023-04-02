a9405aac47adf0d2b44388403abb04170591ad70
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-02 12:11:09 +0200

    color-theme-toggler style

--- src/Components/ColorThemeToggle.js
+++ src/Components/ColorThemeToggle.js
@@ -4,14 +4,12 @@ const ColorThemeToggle = () => {
   return (
     <div className='color-theme-toggle'>
         <div className='color-theme-toggle__btns'>
-            <div className='bg'>
-
-            </div>
+            <div className='bg'></div>
             <div className='black'>
-            <div className='dot'></div>
+                <div className='dot'></div>
             </div>
             <div className='color'>
-            <div className='dot'></div>
+                <div className='dot'></div>
             </div>
         </div>
         <p className='color-theme-toggle__title'>
