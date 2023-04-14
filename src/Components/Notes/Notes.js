44793674e0f0d5cc18a36b8c88071db65bd1b100
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-14 14:29:59 +0200

    icons-36 style

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../../Styles/Components_Styles/Notes/Notes.css';
+import { IoAdd } from "react-icons/io5";
 import { connect } from "react-redux";
 
 const Notes = ( {activeNote, allNotes} ) => {
   return (
     <section className='notes'>
       <div className='notes__left'>
+        <div className='notes__left__list'>
 
+        </div>
+        <div className='notes__left__nav'> 
+          <div className='notes__left__nav__scroll-btns'>
+
+          </div>
+          <button className='icon-36'>
+            <IoAdd />
+          </button>
+        </div>
       </div>
       <div className='notes__right'>
 
