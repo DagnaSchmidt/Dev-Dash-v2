b010cea3566dc5bba1eb0e481c481efb51207fc5
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-21 12:11:55 +0200

    weather card props

--- src/Components/Weather/WeatherForecastCards.js
+++ src/Components/Weather/WeatherForecastCards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import WeatherCard from './WeatherCard';
@@ -6,29 +6,73 @@ import WeatherNavBtn from './WeatherNavBtn';
 const KEY = process.env.REACT_APP_GEODB_KEY
 
 const WeatherForecastCards = ({latitude, longitude}) => {
-    const [weatherForecastData, setWeatherForecastData] = useState();
+    const [weatherForecastData, setWeatherForecastData] = useState([]);
+    const [currentTemp, setCurrentTemp] = useState();
 
-    const options = {
-        method: 'GET',
-        url: `https://foreca-weather.p.rapidapi.com/forecast/daily/${longitude},${latitude}`,
-        params: {alt: '0', tempunit: 'C', windunit: 'MS', periods: '8', dataset: 'full'},
-        headers: {
-            'X-RapidAPI-Key': KEY,
-            'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
-        }
-    };
-    axios.request(options).then(function (response) {
-        const json = response.data;
-        console.log(response.data);  //-> delete later
-        setWeatherForecastData(json);
-    }).catch(function (error) {
-        console.error(error);
-    });
+    useEffect(() => {
+        const weekForecast = {
+            method: 'GET',
+            url: `https://foreca-weather.p.rapidapi.com/forecast/daily/${longitude},${latitude}`,
+            params: {alt: '0', tempunit: 'C', windunit: 'MS', periods: '8', dataset: 'full'},
+            headers: {
+                'X-RapidAPI-Key': KEY,
+                'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+            }
+        };
+        axios.request(weekForecast).then(function (response) {
+            const json = response.data.forecast;
+            console.log(response.data.forecast);  //-> delete later
+            setWeatherForecastData(json);
+        }).catch(function (error) {
+            console.error(error);
+        });
+        const currentWeather = {
+            method: 'GET',
+            url: `https://foreca-weather.p.rapidapi.com/current/${longitude},${latitude}`,
+            params: {alt: '0', tempunit: 'C', windunit: 'MS', tz: 'Europe/London', lang: 'en'},
+            headers: {
+                'X-RapidAPI-Key': KEY,
+                'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+            }
+        };
+        axios.request(currentWeather).then(function (response) {
+            const json = response.data.current.temperature;
+            setCurrentTemp(json);
+            console.log(response.data.current.temperature);  //-> delete later
+        }).catch(function (error) {
+            console.error(error);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    
+
+    const weatherCards = weatherForecastData.map((item) => {
+        return (
+            <WeatherCard
+                currentTemp={currentTemp}
+                {...item}
+                // maxTemp={item.maxTemp}
+                // minTemp={item.minTemp}
+                // sunrise={item.sunrise}
+                // sunset={item.sunset}
+                // symbolPhrase={item.symbolPhrase}
+                // pressure={item.pressure}
+                // cloudiness={item.cloudiness}
+                // maxRelHumidity={item.maxRelHumidity}
+                // minRelHumidity={item.minRelHumidity}
+                // maxWindSpeed={item.maxWindSpeed}
+                // minWindSpeed={item.minWindSpeed}
+                // precipProb={item.precipProb}
+                // precipAccum={item.precipAccum}
+                // uvIndex={item.uvIndex}
+            />
+        )
+    })
 
   return (
     <>
         <div className='weather-forecast-cards'>
-
+            {weatherForecastData.length != 0 && weatherCards}
         </div>
         <div className='weather-forecast-nav'>
 
