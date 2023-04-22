aa720c78bd5650930860838900b2b708a1090db8
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-22 22:18:08 +0200

    name of day displayed on nav btns

--- src/Components/Weather/WeatherNavBtn.js
+++ src/Components/Weather/WeatherNavBtn.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import { SET_DISPLAYED_DAY } from '../../actions';
 
 const WeatherNavBtn = ( {date, maxTemp, minTemp, displayedDay, setDisplayedDay} ) => {
+    const weekday = ["Sun", "Mon","Tue","Wed","Thur","Fri","Sat"];
+    const day = new Date(date).getDay();
   return (
     <div className='weather-nav-btn' style={{backgroundColor: displayedDay === date ? '#E7E7E7' : 'transparent'}} onClick={() => setDisplayedDay(date)}>
-        <h5>Mon</h5>
+        <h5 className='title-medium '>{weekday[day]}</h5>
         <p>{maxTemp}<span>o</span>/{minTemp}<span>o</span></p>
     </div>
   )
