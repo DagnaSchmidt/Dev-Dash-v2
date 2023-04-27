c1edca2a0e69e8b76e54d2cdc7b3c28f4039a7ce
Dagna Schmidt <dagdagi889@gmail.com>
2023-04-27 09:11:27 +0200

    weather saved localizations card content

--- src/Components/Weather/WeatherSavedLocalizationsCard.js
+++ src/Components/Weather/WeatherSavedLocalizationsCard.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { ADD_WEATHER_LOCALIZATION } from '../../actions';
 
-const WeatherSavedLocalizationsCard = () => {
+const WeatherSavedLocalizationsCard = ({ savedLocalizations }) => {
+    console.log(savedLocalizations);
   return (
-    <div>WeatherSavedLocalizationsCard</div>
+    <div className='weather__saved-localizations'>
+        <h5>List of your localizations:</h5>
+        <p>You can save maximum of 13 localizations.</p>
+        <div className='weather__saved-localizations__btns'>
+            {savedLocalizations.length !== 0 &&
+                savedLocalizations.map((item) => {
+                    return (
+                        <button key={item.date}>
+                            {item.city}
+                        </button>
+                    )
+                })
+            }
+        </div>
+    </div>
   )
 }
 
-export default WeatherSavedLocalizationsCard
+const mapStateToProps = store => {
+    return { 
+        savedLocalizations: store.activeUser.weather.savedLocalizations
+    };
+  };
+
+const mapDispatchToProps = dispatch => {
+    return {
+      addWeatherLocalization: (latitude, longitude, city, country) => dispatch({type: ADD_WEATHER_LOCALIZATION, payload: {latitude, longitude, city, country}})
+    };
+  }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherSavedLocalizationsCard);
