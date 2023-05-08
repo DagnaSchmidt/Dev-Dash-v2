93a94edfdefd695eaef5b1746ba8e42847446ac6
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-08 11:29:02 +0200

    fetch days function

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -52,6 +52,7 @@ const Calendar = ({setDisplayedDate, setDisplayedMonth, setDisplayedYear, active
                     return (
                         <Month
                             key={item}
+                            month={item}
                         />
                     )
                 })}
--- src/Components/Calendar/Month.js
+++ src/Components/Calendar/Month.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CalendarDates from "calendar-dates";
 
 
-const Month = ({key}) => {
+const Month = ({month}) => {
     const calendarDates = new CalendarDates();
     const daysNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const monthDays = [];
+
+    const fetchDays = async () => {
+        for (const meta of await calendarDates.getDates(new Date(2023, month))) {
+            monthDays.push(meta);
+          }
+    }
+
+    useEffect(() => {
+        fetchDays();
+        console.log(monthDays);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
   return (
     <div className='month'>
@@ -13,6 +26,13 @@ const Month = ({key}) => {
                 <p className='month__day-name body-medium' key={item}>{item}</p>
             )
         })}
+        {/* {monthDays.length !== 0 &&
+            monthDays.map((item) => {
+                return (
+                    <p key={item.iso}>{item.date}</p>
+                )
+            })
+        } */}
     </div>
   )
 }
