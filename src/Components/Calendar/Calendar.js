2b8d7a4489a2e996b9e267b2a9054bf4773a8d9f
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-13 12:24:04 +0200

    display active month btns functions added

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 import '../../Styles/Components_Styles/Calendar/Calendar.css';
 import Month from './Month';
@@ -9,6 +9,9 @@ const Calendar = ({setDisplayedDate, setDisplayedMonth, setDisplayedYear, active
     const month = new Date().getMonth();
     const date = new Date().getDate();
     const year = new Date().getFullYear();
+
+    const [isNextDisabled, setIsNextDisabled] = useState(false);
+    const [isPrevDisabled, setIsPrevDisabled] = useState(true);
 
     useEffect(() =>{
         setDisplayedYear(year);
@@ -22,11 +25,42 @@ const Calendar = ({setDisplayedDate, setDisplayedMonth, setDisplayedYear, active
     const allMonths = [];
     for(let i = month; allMonths.length < 12; i++){
         allMonths.push(i);
-        if(i === 12){
+        if(i === 11){
             i = -1;
         }
     }
     console.log(allMonths);
+    // console.log(allMonths.length);
+    console.log(activeMonth);
+
+    const index = allMonths.indexOf(activeMonth);
+
+    const next = () => {
+        if(index === 11){}else{
+            const newMonth = allMonths[index +1];
+            setDisplayedMonth(newMonth);
+        }
+    }
+
+    const prev = () => {
+        if(index === 0){}else{
+            const newMonth = allMonths[index -1];
+            setDisplayedMonth(newMonth);
+        }
+    }
+
+    useState(() => {
+        if(activeMonth === allMonths[0]){
+            setIsPrevDisabled(true);
+        }else{
+            setIsPrevDisabled(false);
+        }
+        if(activeMonth === allMonths[11]){
+            setIsNextDisabled(true);
+        }else{
+            setIsNextDisabled(false);
+        }
+    }, [activeMonth])
 
   return (
     <section className='calendar'>
@@ -34,10 +68,10 @@ const Calendar = ({setDisplayedDate, setDisplayedMonth, setDisplayedYear, active
             <div className='calendar__left__displayed-month'>
                 <h5 className='calendar__left__displayed-month__title title-large'>{monthsNames[activeMonth]}</h5>
                 <div className='calendar__left__displayed-month__nav'>
-                    <button className='scroll-btn'>
+                    <button className='scroll-btn' disabled={isPrevDisabled} onClick={() => prev()}>
                         <IoChevronBack />
                     </button>
-                    <button className='scroll-btn'>
+                    <button className='scroll-btn' disabled={isNextDisabled} onClick={() => next()}>
                         <IoChevronForward />
                     </button>
                 </div>
